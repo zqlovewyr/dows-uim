0af253d4f54bd7441702423e0d7023c5a6dc6a34
geeker.lait <lait.zhang@gmail.com>
2022-11-22 09:54:12 +0800

    fix

---
+++ uim-application/src/main/java/org/dows/account/UimApplication.java
@@ -0,0 +1,11 @@
+package org.dows.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UimApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UimApplication.class, args);
+    }
+}
