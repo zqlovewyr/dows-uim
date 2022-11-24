52385379fc5e2b30f5735d8f375d9301abd8c0fe
geeker.lait <lait.zhang@gmail.com>
2022-11-24 14:35:48 +0800

    fix index forward

--- uim-application/uim-application-admin/src/main/java/org/dows/uim/admin/UimAdminApplication.java
+++ uim-application/uim-application-admin/src/main/java/org/dows/uim/admin/UimAdminApplication.java
@@ -1,11 +1,25 @@
 package org.dows.uim.admin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication(scanBasePackages={"org.dows.*"})
 public class UimAdminApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(UimAdminApplication.class, args);
     }
+
+    @Value("${spring.application.index}")
+    private String index ;
+
+    @RequestMapping("/")
+    public String index(){
+        return index;
+    }
+
 }
