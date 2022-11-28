9dfd4d011a4a0863f4e9d1d0bc0150903b85754c
夏海 <xhai@findsoft.com.cn>
2022-11-28 09:14:37 +0000

    1128 daily commit

--- dows-account/account-biz/src/main/java/org/dows/account/biz/enums/EnumAccountStatusCode.java
+++ dows-account/account-biz/src/main/java/org/dows/account/biz/enums/EnumAccountStatusCode.java
@@ -11,6 +11,11 @@ import org.dows.framework.api.StatusCode;
 @AllArgsConstructor
 public enum EnumAccountStatusCode implements StatusCode {
     ACCOUNT_EXIST_EXCEPTION(400001, "账号已存在"),
+    BATCH_IMPORT_IDENTIFIER_DUPLICATED(400002, "批量导入数据账户有重复"),
+    ACCOUNT_INSTANCE_DTO_CANNOT_BE_NULL(400003, "账户实例不能为空"),
+    APPID_CANNOT_BE_BLANK(400004, "应用ID不能为空"),
+    IDENTIFIER_CANNOT_BE_BLANK(400004, "账号不能为空"),
+    ACCOUNT_NAME_CANNOT_BE_BLANK(400004, "账号昵称不能为空"),
     ORG_NOT_EXIST_EXCEPTION(400201, "组织不存在");
     final Integer code;
     final String descr;
