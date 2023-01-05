1e0cc0295a3ba3e16b82a793c3271fee890b7a9d
jiangxia <15228798301@163.com>
2023-01-05 11:12:26 +0800

    修改

--- dows-account/account-config/src/main/java/org/dows/account/config/AccountDataSourceProperties.java
+++ dows-account/account-config/src/main/java/org/dows/account/config/AccountDataSourceProperties.java
@@ -3,9 +3,7 @@ package org.dows.account.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import java.util.Map;
 
