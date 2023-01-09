9365f5580e04ea3c52ee59cb9be81dba3034e2b9
lait <lait.zhang@gmail.com>
2023-01-09 15:26:37 +0800

    fix user

---
+++ dows-user/user-rest/user-rest-admin/src/main/java/org/dows/user/rest/admin/UserContactRest.java
@@ -0,0 +1,30 @@
+package org.dows.user.rest.admin;
+
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dows.framework.crud.mybatis.MybatisCrudRest;
+import org.dows.user.entity.UserContact;
+import org.dows.user.form.UserContactForm;
+import org.dows.user.service.UserContactService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户-联系人(UserContact)表控制层
+ *
+ * @author lait.zhang
+ * @since 2022-10-22 10:31:12
+ */
+@Api(tags = "用户-联系人")
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("userContact")
+public class UserContactRest implements MybatisCrudRest<UserContactForm, UserContact, UserContactService> {
+
+    //private final UserContactBiz userContactBiz;
+
+}
+
