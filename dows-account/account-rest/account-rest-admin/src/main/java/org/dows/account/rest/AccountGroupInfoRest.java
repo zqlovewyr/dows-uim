925dfab02d5d9a2ce94262b8d980875993ba2b3c
jiangxia <15228798301@163.com>
2023-01-13 14:12:45 +0800

    测试接口

--- dows-account/account-rest/account-rest-admin/src/main/java/org/dows/account/rest/AccountGroupInfoRest.java
+++ dows-account/account-rest/account-rest-admin/src/main/java/org/dows/account/rest/AccountGroupInfoRest.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 /**
  * 账号-组(AccountGroupInfo)表控制层
  *
