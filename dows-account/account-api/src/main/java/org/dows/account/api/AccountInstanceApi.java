b68937873d715a8c206f94638e10425200516d82
jiangxia <15228798301@163.com>
2023-01-30 10:29:22 +0800

    账号-实例

---
+++ dows-account/account-api/src/main/java/org/dows/account/api/AccountInstanceApi.java
@@ -0,0 +1,38 @@
+package org.dows.account.api;
+
+import org.dows.account.dto.AccountInstanceDTO;
+import org.dows.account.vo.AccountInstanceVo;
+import org.dows.framework.api.Response;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+/**
+ * 账号-实例维度信息(AccountInstance)Api接口
+ *
+ * @author VX:PN15855012581
+ * @since 2023-01-30 09:36:28
+ */
+public interface AccountInstanceApi {
+
+    /**
+     * runsix method process
+     * 1.check whether accountIdentifier queried by appId & identifier exist
+     * 2.check whether rbacRoleId exist
+     * 3.check whether accountOrgOrgId exist
+     * 4.save accountIdentifier
+     * 5.save accountInstance
+     * 6.save accountRole if rbacRoleId exist
+     * 7.save accountGroup if orgId exist
+     * 8.convert entity to vo and return
+     */
+    Response<AccountInstanceVo> createAccountInstance(AccountInstanceDTO accountInstanceDTO);
+
+
+    Response<List<AccountInstanceDTO>> getAccountInstanceDTOListByFile(MultipartFile file, String appId, Long rbacRoleId, String accountOrgOrgId, String password, String avatar, String source, String phone);
+
+
+    void batchCreateAccountInstance(List<AccountInstanceDTO> accountInstanceDTOList);
+
+    void batchRegister(MultipartFile file, String appId, Long rbacRoleId, String accountOrgOrgId, String password, String avatar, String source, String phone);
+
+}
