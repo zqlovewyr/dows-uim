ffae4c190c9a66a68aab18aaa7bece7a0e96243c
taoyf <1442282810@qq.com>
2023-02-02 14:40:05 +0800

    客户接口目录更换

---
+++ dows-account/account-rest/src/main/java/org/dows/account/rest/tenant/UserAccountRest.java
@@ -0,0 +1,64 @@
+package org.dows.account.rest.tenant;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dows.account.biz.AccountBiz;
+import org.dows.account.query.AccountQuery;
+import org.dows.account.util.MyConstant;
+import org.dows.account.vo.AccountVo;
+import org.dows.framework.api.Response;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@Api(tags = "B端-API-客户")
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("tenant/user/account")
+public class UserAccountRest {
+
+    private final AccountBiz accountBiz;
+
+    @GetMapping("test")
+    public Response test() {
+        return Response.ok("success");
+    }
+
+    @GetMapping("page")
+    @ApiOperation(value = "分页获取客户列表")
+    public Response<IPage<AccountVo>> pageList(
+            @ApiParam(value = "页码", required = true) Integer page,
+            @ApiParam(value = "每页数据条数", required = true) Integer size,
+            @ApiParam(value = "注册开始时间yyyy-MM-dd") @RequestParam(required = false) @DateTimeFormat(pattern = MyConstant.DEFAULT_DATE_FORMAT) LocalDate createStartDate,
+            @ApiParam(value = "注册截止时间yyyy-MM-dd") @RequestParam(required = false) @DateTimeFormat(pattern = MyConstant.DEFAULT_DATE_FORMAT) LocalDate createEndDate,
+            @ApiParam(value = "生日开始时间yyyy-MM-dd") @RequestParam(required = false) @DateTimeFormat(pattern = MyConstant.DEFAULT_DATE_FORMAT) LocalDate birthdayStartDate,
+            @ApiParam(value = "生日截止时间yyyy-MM-dd") @RequestParam(required = false) @DateTimeFormat(pattern = MyConstant.DEFAULT_DATE_FORMAT) LocalDate birthdayEndDate,
+            @ApiParam(value = "是否筛选信息全的客户") @RequestParam(required = false, defaultValue = "false") Boolean infoIfFull,
+            @ApiParam(value = "消费金额排序true升序false降序") @RequestParam(required = false) Boolean moneySort,
+            @ApiParam(value = "最后下单时间排序true升序false降序") @RequestParam(required = false) Boolean lastOrderTimeSort){
+        AccountQuery accountQuery = new AccountQuery();
+        accountQuery.setPage(page);
+        accountQuery.setSize(size);
+        accountQuery.setBirthdayStartDate(birthdayStartDate);
+        accountQuery.setBirthdayEndDate(birthdayEndDate);
+        accountQuery.setCreateStartDate(createStartDate);
+        accountQuery.setCreateEndDate(createEndDate);
+        accountQuery.setInfoIfFull(infoIfFull);
+        accountQuery.setMoneySort(moneySort);
+        accountQuery.setLastOrderTimeSort(lastOrderTimeSort);
+        return accountBiz.getAccuntListPage(accountQuery);
+    }
+
+    @GetMapping("{accountId}/info")
+    @ApiOperation(value = "根据客户账号id获取详情")
+    public Response<AccountVo> getInfo(
+            @ApiParam(required = true, value = "账号id") @PathVariable("accountId") String accountId){
+        return accountBiz.getInfoByAccountId(accountId);
+    }
+}
